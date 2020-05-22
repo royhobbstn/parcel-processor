c425b4c392b8820dc35a4c5becf792a3c3877015
Ubuntu <ubuntu@ip-172-31-37-136.us-east-2.compute.internal>
2020-05-22 04:20:45 +0000

    c9 not going to work

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { subscribeToTimer } from './test';
+
+
+class App extends Component {
+
+	constructor(props) {
+	  super(props);
+
+	  subscribeToTimer((err, timestamp) => {
+	    if(err) {
+	      console.log(err);
+	    }
+	    this.setState({ 
+		    timestamp 
+	    });
+	  });
+	}
+
+	state = {
+		timestamp: 'no timestamp yet'
+	};
+
+	render() {
+	  return (
+		<div className="App">
+		  <p className="App-intro">
+		  This is the timer value: {this.state.timestamp}
+		  </p>
+		</div>
+	  );
+	}
+}
+
+export default App;
