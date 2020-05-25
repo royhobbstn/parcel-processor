fd4283ec8910b7ab06661d7360ec7c841bf62df7
Daniel Trone <danieljtrone@gmail.com>
2020-05-25 07:42:06 -0500

    processes shapefile or geodatabase

--- util/processFile.js
+++ util/processFile.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const chalk = require('chalk');
+const prompt = require('prompt');
 const gdal = require('gdal-next');
 const { getGeoJsonFromGdalFeature } = require('./parseFeature');
 const { StatContext } = require('./StatContext');
@@ -6,13 +8,17 @@ const { outputDir, unzippedDir } = require('./constants');
 
 // fileName is the file name without the extension
 // fileType supports 'shapefile' and 'geodatabase'
-exports.processFile = function (fileName, fileType) {
+exports.processFile = async function (fileName, fileType) {
   console.log(`Found file: ${fileName}`);
 
   console.log({ fileName, fileType });
-  console.log('opening from ' + `${unzippedDir}/${fileName}`);
+  console.log(
+    'opening from ' + `${unzippedDir}/${fileName + (fileType === 'shapefile' ? '.shp' : '')}`,
+  );
 
-  const dataset = gdal.open(`${unzippedDir}/${fileName + fileType === 'shapefile' ? '.shp' : ''}`);
+  const dataset = gdal.open(
+    `${unzippedDir}/${fileName + (fileType === 'shapefile' ? '.shp' : '')}`,
+  );
 
   const driver = dataset.driver;
   const driver_metadata = driver.getMetadata();
@@ -24,25 +30,49 @@ exports.processFile = function (fileName, fileType) {
 
   console.log(`Driver = ${driver.description}\n`);
 
-  let i = 0; // iterator for layer labels
+  let total_layers = 0; // iterator for layer labels
 
   // print out info for each layer in file
   dataset.layers.forEach(layer => {
-    console.log(`${i++}: ${layer.name}`);
+    console.log(chalk.bold.red(`#${total_layers++}: ${layer.name}`));
     console.log(`  Geometry Type = ${gdal.Geometry.getName(layer.geomType)}`);
-    console.log(`  Spatial Reference = ${layer.srs ? layer.srs.toWKT() : 'null'}`);
+    console.log(chalk.dim(`  Spatial Reference = ${layer.srs ? layer.srs.toWKT() : 'null'}`));
     console.log('  Fields: ');
     layer.fields.forEach(field => {
       console.log(`    -${field.name} (${field.type})`);
     });
-    console.log(`  Feature Count = ${layer.features.count()}\n`);
+    console.log(chalk.blue(`  Feature Count = ${layer.features.count()}\n`));
   });
 
-  // if just one layer, use it
-  // otherwise, prompt for layer number
+  let CHOSEN_TABLE = 0;
 
-  // TODO hardcoding for now
-  const CHOSEN_TABLE = fileType === 'shapefile' ? 0 : 1;
+  if (total_layers !== 1) {
+    // if just one layer, use it
+    // otherwise, prompt for layer number
+
+    try {
+      const table_choice = await new Promise((resolve, reject) => {
+        prompt.start();
+
+        prompt.get(['layer'], function (err, result) {
+          if (err) {
+            reject(err);
+          }
+          console.log('Command-line input received:');
+          console.log('  layer: ' + result.layer);
+          resolve(result.layer);
+        });
+      });
+      CHOSEN_TABLE = parseInt(table_choice);
+    } catch (e) {
+      console.error('ERROR getting user input');
+      console.error(e);
+      process.exit();
+    }
+
+    console.log({ CHOSEN_TABLE });
+  }
+
   const SPLITTER = fileType === 'shapefile' ? '.shp' : '.gdb';
 
   const statCounter = new StatContext();
