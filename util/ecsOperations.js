c543fdb0101538fee9e8fc349fe50e2aedb33666
Daniel Trone <danieljtrone@gmail.com>
2020-07-17 12:49:26 -0500

    cloudwatch logs

--- util/ecsOperations.js
+++ util/ecsOperations.js
@@ -1,6 +1,7 @@
 // @ts-check
 
 const AWS = require('aws-sdk');
+const config = require('config');
 const ecs = new AWS.ECS({ apiVersion: '2014-11-13', region: 'us-east-2' });
 const { unwindStack } = require('./misc');
 
@@ -74,93 +75,35 @@ function describeTasks(ctx, taskArns) {
   });
 }
 
-// TODO
+exports.runProcessorTask = runProcessorTask;
 
-// runTask
+async function runProcessorTask(ctx) {
+  ctx.process.push('runProcessorTask');
 
-// startTask
+  const taskDefinition = config.get('ECS.taskDefinition');
 
-const forReference = {
-  tasks: [
-    {
-      attachments: [
-        {
-          id: '36643d5f-1941-4da5-9d44-8f979fac1bd4',
-          type: 'ElasticNetworkInterface',
-          status: 'ATTACHED',
-          details: [
-            {
-              name: 'subnetId',
-              value: 'subnet-2334cf48',
-            },
-            {
-              name: 'networkInterfaceId',
-              value: 'eni-05a471f4c22b7426a',
-            },
-            {
-              name: 'macAddress',
-              value: '02:4c:64:04:a6:b0',
-            },
-            {
-              name: 'privateIPv4Address',
-              value: '172.31.13.176',
-            },
-          ],
-        },
-      ],
-      availabilityZone: 'us-east-2a',
-      capacityProviderName: 'FARGATE',
-      clusterArn: 'arn:aws:ecs:us-east-2:000009394762:cluster/parcel-processor',
-      connectivity: 'CONNECTED',
-      connectivityAt: '2020-07-16T22:49:27.711Z',
-      containers: [
-        {
-          containerArn:
-            'arn:aws:ecs:us-east-2:000009394762:container/2ef1a131-b53f-4a04-9c32-d0675d3bd0ac',
-          taskArn: 'arn:aws:ecs:us-east-2:000009394762:task/ecf1754d-b239-4f35-8a48-a5b73304660b',
-          name: 'processor-dev',
-          image: '000009394762.dkr.ecr.us-east-2.amazonaws.com/parcel-outlet:latest',
-          imageDigest: 'sha256:8e115042a914d436690db171d3fc5bdfd5785ac43ba02b5cd851533399655cb8',
-          runtimeId: 'ecf1754d-b239-4f35-8a48-a5b73304660b-1500111173',
-          lastStatus: 'RUNNING',
-          networkBindings: [],
-          networkInterfaces: [
-            {
-              attachmentId: '36643d5f-1941-4da5-9d44-8f979fac1bd4',
-              privateIpv4Address: '172.31.13.176',
-            },
-          ],
-          healthStatus: 'UNKNOWN',
-          cpu: '2048',
-          memory: '8000',
-          memoryReservation: '6000',
-        },
-      ],
-      cpu: '2048',
-      createdAt: '2020-07-16T22:49:24.171Z',
-      desiredStatus: 'RUNNING',
-      group: 'family:processor-dev',
-      healthStatus: 'UNKNOWN',
-      lastStatus: 'RUNNING',
+  return new Promise((resolve, reject) => {
+    const params = {
+      taskDefinition,
+      cluster: 'parcel-processor',
+      count: 1,
       launchType: 'FARGATE',
-      memory: '8192',
-      overrides: {
-        containerOverrides: [
-          {
-            name: 'processor-dev',
-          },
-        ],
-        inferenceAcceleratorOverrides: [],
-      },
       platformVersion: '1.4.0',
-      pullStartedAt: '2020-07-16T22:49:42.853Z',
-      pullStoppedAt: '2020-07-16T22:51:01.853Z',
-      startedAt: '2020-07-16T22:51:11.853Z',
-      tags: [],
-      taskArn: 'arn:aws:ecs:us-east-2:000009394762:task/ecf1754d-b239-4f35-8a48-a5b73304660b',
-      taskDefinitionArn: 'arn:aws:ecs:us-east-2:000009394762:task-definition/processor-dev:16',
-      version: 4,
-    },
-  ],
-  failures: [],
-};
+      networkConfiguration: {
+        awsvpcConfiguration: {
+          subnets: ['subnet-c0db438c', 'subnet-2334cf48', 'subnet-a1cafbdb'],
+          assignPublicIp: 'ENABLED',
+          securityGroups: ['sg-abaf0fd3'],
+        },
+      },
+    };
+
+    ecs.runTask(params, function (err, data) {
+      if (err) {
+        return reject(err);
+      }
+      unwindStack(ctx.process, 'runProcessorTask');
+      return resolve(data);
+    });
+  });
+}
